4635369f3ba7c0223c0b15c0a955cfe27e407f28
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-02-22 21:21:35 +0100

    Fix devtools::test() errors due to 2 new fields being returned from the API.
    -----------
    -----------
    Before Fix:
    
    ℹ Testing ROpenFIGI
    ✔ | F W S  OK | Context
    ✖ | 2       2 | testOpenFIGI [4.5s]
    ──────────────────────────────────────────────────────────────────────────────────────────────────────
    Failure (testOpenFIGI.R:10): number of returned json list
    names(jsonrst[[1L]]) not equal to c(...).
    Lengths differ: 12 is not 10
    
    Failure (testOpenFIGI.R:16): number of returned json list
    ncol(finalrst) not equal to 30L.
    1/1 mismatches
    [1] 32 - 30 == 2
    ──────────────────────────────────────────────────────────────────────────────────────────────────────
    
    ══ Results ═══════════════════════════════════════════════════════════════════════════════════════════
    Duration: 4.6 s
    
    [ FAIL 2 | WARN 0 | SKIP 0 | PASS 2 ]
    
    -----------
    -----------
    After Fix:
    
    ℹ Testing ROpenFIGI
    ✔ | F W S  OK | Context
    ✔ |         4 | testOpenFIGI [5.1s]
    
    ══ Results ═══════════════════════════════════════════════════════════════════════════════════════════
    Duration: 5.1 s
    
    [ FAIL 0 | WARN 0 | SKIP 0 | PASS 4 ]

--- tests/testthat/testOpenFIGI.R
+++ tests/testthat/testOpenFIGI.R
@@ -5,14 +5,18 @@ jsondf <- sampleOpenFIGIdf()
 
 jsonrst <- OpenFIGI(jsondf)
 
-test_that('number of returned json list', {
-  expect_equal(length(jsonrst), 2L )
-  expect_equal(names(jsonrst[[1L]]), c('figi','securityType','marketSector','ticker','name','uniqueID','exchCode','shareClassFIGI','compositeFIGI','uniqueIDFutOpt') )
+test_that("number of returned json list", {
+  expect_equal(length(jsonrst), 2L)
+  expect_equal(names(jsonrst[[1L]]),
+               c("figi", "name", "ticker",
+                 "exchCode", "compositeFIGI", "uniqueID",
+                 "securityType", "marketSector", "shareClassFIGI",
+                 "uniqueIDFutOpt", "securityType2", "securityDescription"))
 })
 
 finalrst <- OpenFIGI_MappingCreator(jsondf)
 
-test_that('number of returned json list', {
-  expect_equal(ncol(finalrst), 30L )
+test_that("number of returned json list", {
+  expect_equal(ncol(finalrst), 32L )
   expect_equal(finalrst$ID_ISIN[1L], "US4592001014" )
 })
