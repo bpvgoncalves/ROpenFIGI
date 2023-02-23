d0903d6cabebfae31ef15ac5dd26b97839b5defe
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-02-23 21:50:48 +0100

    Update tests

--- tests/testthat/testOpenFIGI.R
+++ tests/testthat/testOpenFIGI.R
@@ -25,14 +25,24 @@ test_that("connections via proxy work", {
 
   # The following web proxy was available at 'https://hidemy.name/en/proxy-list/' as of the
   # written of this test. This may need to be changed overtime.
-  p <- httr::use_proxy("5.75.134.209", 40000)
+  p <- httr::use_proxy("157.245.27.9", 3128)
   # Do not run in CRAN, just in case the proxy becomes unavailable.
   skip_on_cran()
 
   jsonrst2 <- OpenFIGI(jsondf, proxy = p)
   expect_identical(jsonrst, jsonrst2)
 
+  jsonrst3 <- OpenFIGI(jsondf, proxy = "auto")
+  expect_identical(jsonrst, jsonrst3)
+
+  expect_error(OpenFIGI(jsondf, proxy = "bad_input"))
+
   finalrst2 <- OpenFIGI_MappingCreator(jsondf, proxy = p)
   expect_identical(finalrst, finalrst2)
+
+  finalrst3 <- OpenFIGI_MappingCreator(jsondf, proxy = "auto")
+  expect_identical(finalrst, finalrst3)
+
+  expect_error(OpenFIGI_MappingCreator(jsondf, proxy = "bad_input"))
 
 })
